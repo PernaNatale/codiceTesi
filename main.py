3124502fc6cc85f4e4144323b3480165025a6c01
PernaNatale <62794769+PernaNatale@users.noreply.github.com>
2024-03-04 18:20:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import re
 
 def calcola_offensivita(frase):
-    parole_chiave = ["no", "don't", "thanks", "thank you", "i prefer", "like", "i rather"]
+    negazione_ringraziamento = ["no", "don't", "thanks", "thank you"]
+    parole_chiave = ["i prefer", "like", "i rather"]
     analyzer = SentimentIntensityAnalyzer()
-    punteggio = analyzer.polarity_scores(frase)['compound']*-3
-    parole_presenti = sum(1 for parola in parole_chiave if parola in frase)
-    if parole_presenti >= 3:
-        punteggio += 1
-    else:
-        punteggio += 0
+    punteggio = analyzer.polarity_scores(frase)['compound']*-1
+    parole_presenti = sum(1 for parola in negazione_ringraziamento if parola in frase)
+    key_words = sum(1 for parola in parole_chiave if parola in frase)
+    if key_words >=1:
+        punteggio +=0.7
+    if parole_presenti >= 2:
+        punteggio += 0.5
     lunghezza_frase = len(re.findall(r'\w+', frase))
     if lunghezza_frase >= 4:
         punteggio += 0.4
-    else:
-        punteggio += 0
     return punteggio
 
-frase_da_valutare = "I don't like discounts"
+frase_da_valutare = "No, i don't feel lucky"
 
 valore_offensivita = calcola_offensivita(frase_da_valutare.lower())
 
